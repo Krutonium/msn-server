577a3a249eadd50aa18efb247412f3373c8a1cc0
valtron <valtron2000@gmail.com>
2017-07-22 18:21:02 -0600

    wip: xml files for almost every endpoint

--- ctrl_auth.py
+++ ctrl_auth.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from urllib.parse import unquote
+import lxml
 import jinja2
 from aiohttp import web
 from random import random
@@ -30,8 +31,9 @@ def create_app(user_service, auth_service):
 	app.router.add_post('/NotRST.srf', handle_not_rst)
 	
 	# MSN 8.1.0178
-	app.router.add_post('/abservice/SharingService.asmx', handle_soap)
-	app.router.add_post('/abservice/abservice.asmx', handle_soap)
+	app.router.add_post('/abservice/SharingService.asmx', handle_abservice)
+	app.router.add_post('/abservice/abservice.asmx', handle_abservice)
+	app.router.add_post('/storageservice/SchematizedStore.asmx', handle_storageservice)
 	
 	# Misc
 	app.router.add_get('/etc/debug', handle_debug)
@@ -57,37 +59,102 @@ async def on_response_prepare(req, res):
 async def handle_debug(req):
 	return render(req, 'debug.html')
 
-async def handle_soap(req):
-	action, user = await _preprocess_soap(req)
+async def handle_abservice(req):
+	header, action, user = await _preprocess_soap(req)
 	if user is None:
 		return web.Response(status = 403, text = '')
-	if bool(action.find('.//{http://www.msn.com/webservices/AddressBook}deltasOnly')):
-		return render(req, 'fault.fullsync.xml')
 	action_str = _get_tag_localname(action)
-	detail = req.app['user_service'].get_detail(user.uuid)
+	if bool(action.find('.//{http://www.msn.com/webservices/AddressBook}deltasOnly')):
+		return render(req, 'abservice/Fault.fullsync.xml', { 'faultactor': action_str })
 	now_str = datetime.utcnow().isoformat() + 'Z'
+	detail = req.app['user_service'].get_detail(user.uuid)
+	
 	if action_str == 'FindMembership':
-		return render(req, 'SharingService.xml', {
+		return render(req, 'abservice/FindMembershipResponse.xml', {
 			'user': user,
 			'detail': detail,
 			'lists': [models.Lst.AL, models.Lst.BL, models.Lst.RL, models.Lst.PL],
 			'now': now_str,
-			'random_crap': str(random())[2:],
 		})
-	elif action_str == 'ABFindAll':
-		return render(req, 'abservice.xml', {
+	if action_str == 'AddMember':
+		# TODO
+		return render(req, 'abservice/AddMemberResponse.xml')
+	if action_str == 'DeleteMember':
+		# TODO
+		return render(req, 'abservice/DeleteMemberResponse.xml')
+	
+	if action_str == 'ABFindAll':
+		return render(req, 'abservice/ABFindAllResponse.xml', {
 			'user': user,
 			'detail': detail,
 			'Lst': models.Lst,
 			'list': list,
 			'now': now_str,
-			'random_crap': str(random())[2:],
 		})
-	else:
-		import lxml
-		print("Unknown SOAP:")
+	if action_str == 'ABContactAdd':
+		# TODO
+		return render(req, 'abservice/ABContactAddResponse.xml')
+	if action_str == 'ABContactDelete':
+		# TODO
+		return render(req, 'abservice/ABContactDeleteResponse.xml')
+	if action_str == 'ABGroupAdd':
+		# TODO
+		return render(req, 'abservice/ABGroupAddResponse.xml')
+	if action_str == 'ABGroupUpdate':
+		# TODO
+		return render(req, 'abservice/ABGroupUpdateResponse.xml')
+	if action_str == 'ABGroupDelete':
+		# TODO
+		return render(req, 'abservice/ABGroupDeleteResponse.xml')
+	if action_str == 'ABGroupContactAdd':
+		# TODO
+		return render(req, 'abservice/ABGroupContactAddResponse.xml')
+	if action_str == 'ABGroupContactDelete':
+		# TODO
+		return render(req, 'abservice/ABGroupContactDeleteResponse.xml')
+	
+	return _unknown_soap(req, header, action)
+
+async def handle_storageservice(req):
+	header, action, user = await _preprocess_soap(req)
+	action_str = _get_tag_localname(action)
+	now_str = datetime.utcnow().isoformat() + 'Z'
+	if action_str == 'GetProfile':
+		return render(req, 'storageservice/GetProfileResponse.xml', {
+			'user': user,
+			'now': now_str,
+		})
+	if action_str == 'FindDocuments':
+		# TODO
+		return render(req, 'storageservice/FindDocumentsResponse.xml', {
+			'user': user,
+		})
+	if action_str == 'UpdateProfile':
+		# TODO
+		return render(req, 'storageservice/UpdateProfileResponse.xml')
+	if action_str == 'DeleteRelationships':
+		# TODO
+		return render(req, 'storageservice/DeleteRelationshipsResponse.xml')
+	if action_str == 'CreateDocument':
+		# TODO
+		return render(req, 'storageservice/CreateDocumentResponse.xml', {
+			'user': user,
+		})
+	if action_str == 'CreateRelationships':
+		# TODO
+		return render(req, 'storageservice/CreateRelationshipsResponse.xml')
+	if action_str in { 'ShareItem' }:
+		# TODO
+		return _unknown_soap(req, header, action, expected = True)
+	return _unknown_soap(req, header, action)
+
+def _unknown_soap(req, header, action, *, expected = False):
+	action_str = _get_tag_localname(action)
+	if not expected and settings.DEBUG:
+		print("Unknown SOAP:", action_str)
+		print(lxml.etree.tostring(header, pretty_print = True).decode('utf-8'))
 		print(lxml.etree.tostring(action, pretty_print = True).decode('utf-8'))
-		return render(req, 'fault.xml', { 'action': action_str })
+	return render(req, 'Fault.unsupported.xml', { 'faultactor': action_str })
 
 async def _preprocess_soap(req):
 	from lxml.objectify import fromstring as parse_xml
@@ -97,23 +164,23 @@ async def _preprocess_soap(req):
 	
 	user = None
 	token = root.find('.//{http://www.msn.com/webservices/AddressBook}TicketToken')
+	if token is None:
+		token = root.find('.//{http://www.msn.com/webservices/storage/w10}TicketToken')
 	if token is not None:
 		auth = req.app['auth_service']
 		uuid = auth.pop_token('contacts', token)
 		if uuid is not None:
 			user = req.app['user_service'].get(uuid)
+			if user is not None:
+				# Refresh the token for later use
+				auth.create_token('contacts', uuid, token = token, lifetime = 24 * 60 * 60)
 	
-	if user is not None:
-		action = root.find('.//{http://schemas.xmlsoap.org/soap/envelope/}Body/*[1]')
-		# Refresh the token for later use
-		auth.create_token('contacts', uuid, token = token, lifetime = 24 * 60 * 60)
-	else:
-		action = None
+	header = root.find('.//{http://schemas.xmlsoap.org/soap/envelope/}Header')
+	action = root.find('.//{http://schemas.xmlsoap.org/soap/envelope/}Body/*[1]')
 	
-	return action, user
+	return header, action, user
 
 def _get_tag_localname(elm):
-	import lxml
 	return lxml.etree.QName(elm.tag).localname
 
 async def handle_msgrconfig(req):
@@ -171,6 +238,8 @@ def _login(req, email, pwd):
 	return req.app['auth_service'].create_token('nb/login', uuid)
 
 async def handle_other(req):
+	if settings.DEBUG:
+		print("! Unknown: {} {}://{}{}".format(req.method, req.scheme, req.host, req.path_qs))
 	return web.Response(status = 404, text = '')
 
 def render(req, tmpl_name, ctxt = None):
