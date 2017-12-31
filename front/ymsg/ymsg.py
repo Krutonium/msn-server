6152e51324de0126071741e55eff6ba041514183
OhHelloThereImTheGuy312 <helloimtheguy312@hotmail.com>
2017-12-31 12:16:19 +0000

    indent fixes

--- front/ymsg/ymsg.py
+++ front/ymsg/ymsg.py
@@ -33,18 +33,18 @@ class YMSGCtrlBase(metaclass = ABCMeta):
 		
 		for y in self.decoder.data_received(data):
 		    
-            # Escargot's MSN and Yahoo functions have similar name structures
-            # MSN: "_m_CMD"
-            # Yahoo: "_y_[hex version of service; a bit nicer than using the service number]
-            
-            try:
-				# check version and vendorId
-				if y[1][0] > 16 or y[1][1] not in (0, 100):
-				    break
-			    f = getattr(self, '_y_{}'.format(binascii.hexlify(struct.pack('!H', y[0])).decode())
-				f(*y[1][0:])
+		    # Escargot's MSN and Yahoo functions have similar name structures
+		    # MSN: "_m_CMD"
+		    # Yahoo: "_y_[hex version of service; a bit nicer than using the service number]
+		    
+		    try:
+		        # check version and vendorId
+		        if y[1][0] > 16 or y[1][1] not in (0, 100):
+		            break
+		        f = getattr(self, '_y_{}'.format(binascii.hexlify(struct.pack('!H', y[0])).decode())
+		        f(*y[1][0:])
 		    except Exception as ex:
-				self.logger.error(ex)
+		        self.logger.error(ex)
 	
 	def send_reply(self, *y) -> None:
 		self.encoder.write(y)
