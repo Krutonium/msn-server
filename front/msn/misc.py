7e3651a27b773ed67558226089f5cd0233a97a87
OhHelloThereImTheGuy312 <helloimtheguy312@hotmail.com>
2018-03-25 15:17:50 -0400

    add OIM-related functions

--- front/msn/misc.py
+++ front/msn/misc.py
@@ -4,7 +4,7 @@ from util.misc import first_in_iterable
 
 from core import error
 from core.backend import Backend
-from core.models import Contact
+from core.models import User, Contact
 
 def build_msnp_presence_notif(trid: Optional[str], ctc: Contact, dialect: int, backend: Backend) -> Iterable[Tuple[Any, ...]]:
 	status = ctc.status
@@ -58,6 +58,47 @@ def encode_email_networkid(email: str, networkid: Optional[int]) -> str:
 def encode_msnobj(msnobj: Optional[str]) -> Optional[str]:
 	if msnobj is None: return None
 	return quote(msnobj, safe = '')
+
+def gen_mail_data(user: User, backend: Backend, *, oim_uuid: Optional[str] = None, just_sent: bool = False, on_ns: bool = True, e_node: bool = True, q_node: bool = True) -> str:
+	md_m_pl = ''
+	if just_sent:
+		oim_collection = backend.user_service.get_oim_single(user.email, oim_uuid)
+	else:
+		oim_collection = backend.user_service.get_oim_batch(user.email)
+	if on_ns and len(oim_collection) > 25: return 'too-large'
+	
+	for oim in oim_collection:
+		md_m_pl += M_MAIL_DATA_PAYLOAD.format(
+			rt = (RT_M_MAIL_DATA_PAYLOAD.format(
+				senttime = (oim.last_oim_sent.isoformat()[:19] + 'Z')
+			) if not just_sent else ''), oimsz = oim.oim_content_length,
+			frommember = oim.from_member_name, guid = oim.run_id, fid = ('00000000-0000-0000-0000-000000000009' if not just_sent else '.!!OIM'),
+			fromfriendly = (oim.from_member_friendly if not just_sent else _format_friendly(oim.from_member_friendly)),
+			su = (SU_M_MAIL_DATA_PAYLOAD if just_sent else ''),
+		)
+	
+	return MAIL_DATA_PAYLOAD.format(
+		e = (E_MAIL_DATA_PAYLOAD if e_node else ''),
+		q = (Q_MAIL_DATA_PAYLOAD if q_node else ''),
+		m = md_m_pl,
+	)
+
+def _format_friendly(friendlyname):
+	friendly_parts = friendlyname.split('?')
+	friendly_parts[3] += ' '
+	return '?'.join(friendly_parts)
+
+MAIL_DATA_PAYLOAD = '''<MD>{e}{q}{m}</MD>'''
+
+E_MAIL_DATA_PAYLOAD = '''<E><I>0</I><IU>0</IU><O>0</O><OU>0</OU></E>'''
+
+Q_MAIL_DATA_PAYLOAD = '''<Q><QTM>409600</QTM><QNM>204800</QNM></Q>'''
+
+M_MAIL_DATA_PAYLOAD = '''<M><T>11</T><S>6</S>{rt}<RS>0</RS><SZ>{oimsz}</SZ><E>{frommember}</E><I>{guid}</I><F>{fid}</F><N>{fromfriendly}</N></M>{su}'''
+
+RT_M_MAIL_DATA_PAYLOAD = '''<RT>{senttime}</RT>'''
+
+SU_M_MAIL_DATA_PAYLOAD = '''<SU> </SU>'''
 
 class Err:
 	InvalidParameter = 201
